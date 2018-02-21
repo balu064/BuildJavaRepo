9bc6798c53d0d50506e68b40dcf7e91160325863
balu064 <balakalletla@gmail.com>
2018-02-20 23:49:17 -0600

    secound change

--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -1,5 +1,5 @@
 public class Human {
 	public static void main(String[] args) {
-	   System.out.println("This is the sample test output");
+	   System.out.println("This is the sample test output 2");
 	}
 }
