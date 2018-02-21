126334ef9f6ec2161c104ea12698a6f4fc47a65d
balu064 <balakalletla@gmail.com>
2018-02-21 00:58:19 -0600

    test

--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -1,5 +1,5 @@
 public class Human {
 	public static void main(String[] args) {
-	   System.out.println("This is the sample test output final out put sample");
+	   System.out.println("Sample output to check for human code");
 	}
 }
